094f6084619ce8b452fa800597b68881f9e8e42f
Alex <alejandro.ramos.gua@gmail.com>
2025-01-10 17:59:14 +0000

    Almost all with c bonus

--- ft_printf_bonus.c
+++ ft_printf_bonus.c
@@ -6,7 +6,7 @@
 /*   By: alex <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/30 09:31:38 by alex              #+#    #+#             */
-/*   Updated: 2025/01/09 20:09:05 by aramos           ###   ########.fr       */
+/*   Updated: 2025/01/10 16:58:02 by aramos           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,14 @@ static void	bonus(const char **str, t_format *format, va_list args, int *printed
 	const char	*cont_str;
 
 	*format = (t_format){0, 0, -1, 0};
+	cont_str = *str;
 	parse_flags(str, format);
 	parse_width(str, format, args);
 	parse_precision(str, format, args);
 	parse_specifier(str, format);
-	cont_str = apply_specifier(str, format, args, printed_chars);
-	ft_printf(cont_str, args);
-
+	apply_specifier(str, format, args, printed_chars);
+	//cont_str = 
+	//ft_printf(cont_str, args);
 }
 
 
@@ -42,7 +43,7 @@ int	ft_printf(const char *str, ...)
 			str++;
 			bonus(&str, &format, args, &printed_chars);
 		}
-		else
+		if (*str != '%')
 		{
 			ft_putchar_fd(*str, 1);
 			printed_chars++;
